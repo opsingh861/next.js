1423062232f24ec45d5965aad9b2b9dc89b5c5c5
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-13 22:11:10 +0530

    event handling by using 'use client'

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-
+'use client' // use this if you want interactivity at client side
 
 export default function Home() {
+  const call = (item) => {
+    alert(item)
+  }
   return (
-    <p>
-      hello world
-    </p>
+    <main>
+      <h1>Click the button</h1>
+      <button onClick={() => call('apple')}>Click me</button>
+    </main>
   )
 }
