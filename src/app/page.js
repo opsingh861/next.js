bb1cc7fed173cc7f4962d0eab0f8ac91709d50eb
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-13 23:26:34 +0530

    Linking and Navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
-'use client' // use this if you want interactivity at client side
+'use client'
+import Link from "next/link"
+
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const call = (item) => {
-    alert(item)
+  const router = useRouter()
+  const changeRoute = (name) => {
+    router.push(name)
   }
   return (
     <main>
-      <h1>Click the button</h1>
-      <button onClick={() => call('apple')}>Click me</button>
+      <h1>Home</h1>
+      <Link href="/login">Go to login page</Link>
+      <br />
+      <br />
+      <button type="button" onClick={() => changeRoute("/about")}>
+        Go to about page
+      </button>
     </main>
   )
 }
+
