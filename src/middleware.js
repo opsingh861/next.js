ca93043a7e5223253ff48c7c251f6c32b854b8d8
Aditya Dhanraj <opsingh861@gmail.com>
2023-12-15 00:51:55 +0530

    redirecting

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,13 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
     console.log(request.nextUrl.pathname)
     if (request.nextUrl.pathname !== "/login") {
-        return NextResponse.redirect(new URL("/login", request.nextUrl))
+        if (request.nextUrl.pathname !== "/about") {
+            return NextResponse.redirect(new URL("/login", request.nextUrl))
+
+        }
+
     }
-
 }
-
 export const config = {
     matcher: ["/about/:path*"]
 }
